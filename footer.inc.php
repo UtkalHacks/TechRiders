aad79e843bb766ecceb880015e95fe99b3411721
rohit2146 <43812547+rohit2146@users.noreply.github.com>
2019-03-17 16:56:17 +0530

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,7 @@
+<?php
+echo '<p></p>';
+echo '<p></p>';
+echo '<p></p>';
+echo '<p class="text-center">&copy;2019-Team: TechRiders. All Rights Reserved.</p>';
+echo '<p class="text-center">Disclaimer: We are not Responsible for the Placement Data Provided In Our Site. </p>';
+?>
